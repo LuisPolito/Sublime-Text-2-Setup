8d2ab9f1ced013736943ff5b277af29a3a015a5e
David Acevedo <rabbit.editor@gmail.com>
2013-02-26 20:20:12 -0400

    cleaned up comments

--- SublimeTextSetUp.rb
+++ SublimeTextSetUp.rb
@@ -1,6 +1,23 @@
 #! /usr/bin/env ruby
 require 'tmpdir'
+require 'optparse'; options = {:coffee_script => false, :test => false, :cleanup => false}
+#consider substituting FileUtils.cp_r w/ FileUtils.install if possible.
 
+option_parser = OptionParser.new("Default Use is no Options.") do |opts|
+  opts.program_name = "Sublime Text 2 Editor Rails Set-up install script."
+  opts.on('-t', '--test', 
+    "Testing only Writes to:\n\t\t\t\t\t #{ENV['HOME']}/.temp dir.") do
+    options[:test] = true
+    options[:cleanup] = true
+    options[:coffee_script] = true
+  end
+  opts.on('-u', '--cleanup', "Removes temp directory contents, default for JRuby") do
+    options[:cleanup] = true
+  end
+end
+option_parser.parse!
+  
+# COLORIZE OUTPUT
 class Msg < String
   def colorize(color_code)
     #from StackOverflow [http://stackoverflow.com/questions/1489183/colorized-ruby-output]
@@ -12,7 +29,7 @@ class Msg < String
   end
 end
 
-#figure out windows? mac? linux? install directories
+## DETERMINE OS windows? mac? linux? install directories
 def set_sublime_dir(system = RUBY_PLATFORM)
   Dir.respond_to?(:home) ? home = Dir.home : home = ENV['HOME']
   case system
@@ -40,10 +57,10 @@ def set_sublime_dir(system = RUBY_PLATFORM)
   [sublime_packages_path, sublime_user_packages_path]
 end
 
-def check_exit_code(message= "#{caller().first}")
+def check_exit_code(exit_code= $?.exitstatus, message= "#{caller().first}")
   #raise if exit code ($?) not good (0)
-  raise message unless $?.to_i === 0 
-  $?.to_i
+  raise message unless exit_code == 0 
+  exit_code
 end
 
 ## Set paths ##
@@ -51,65 +68,76 @@ sublime_packages_path, sublime_user_packages_path = set_sublime_dir
 tempdir_path = Dir.mktmpdir
 puts Msg.new("Downloading to temp dir: #{tempdir_path}").color(:blue)
 Dir.chdir(tempdir_path)
-#Test paths set correctly
-  raise unless File.exists?(sublime_packages_path) 
-  raise unless File.exists?(sublime_user_packages_path) 
-
-#Test if Git is installed or exit
-git_version = %x[git --version]
-  abort("Git Required") unless git_version.match(/^git version [1-9]\.[1-9]\.[1-9]/)
-# rvm_installed = ENV['PATH']['rvm'] #!%x[which rvm].empty?
 
 ################ For Testing Purposes ###################
-# sublime_packages_path = "#{ENV['HOME']}/.temp"
-# sublime_user_packages_path = "#{ENV['HOME']}/.temp"
-# puts Msg.new("#TESTING#").color(:red)
+# options = {:coffee_script => true, :test => true, :cleanup => true}
+if options[:test] == true
+  Dir.mkdir("#{ENV['HOME']}/.temp") unless File.exists?("#{ENV['HOME']}/.temp")
+  sublime_packages_path = "#{ENV['HOME']}/.temp"
+  sublime_user_packages_path = "#{ENV['HOME']}/.temp"
+  puts Msg.new("##TESTING##").color(:red)
+end
 #########################################################
 
-#Install SASS Higlighting
-%x[git clone https://github.com/n00ge/sublime-text-haml-sass.git]
-check_exit_code()
-Dir.chdir("sublime-text-haml-sass") do
-  if RUBY_PLATFORM === "java"
-    FileUtils.mv('SASS', (sublime_packages_path + "/"), :verbose => true, :force => true )
-  else
-    FileUtils.cp_r('SASS', sublime_packages_path)
+## TEST requirements ##
+def test_requirements(sublime_packages_path, sublime_user_packages_path)
+  #Test paths set correctly
+  a1, a2 = sublime_packages_path, sublime_user_packages_path
+  raise "Can't find Sublime dir: #{a1}" unless File.exists?(a1) && File.directory?(a1)
+  raise "Can't find Sublime dir: #{a2}" unless File.exists?(a2) && File.directory?(a2)
+  #Test if Git is installed or exit
+  git_version = %x[git --version]
+  abort("Git Required") unless git_version.match(/^git version [1-9]\.[1-9]\.[1-9]/)
+  # rvm_installed = ENV['PATH']['rvm'] #!%x[which rvm].empty?
+end
+test_requirements(sublime_packages_path, sublime_user_packages_path)
+
+## Install Packages ##
+def install_SASS(sublime_packages_path)#Install SASS Higlighting
+  %x[git clone https://github.com/n00ge/sublime-text-haml-sass.git]
+  check_exit_code()
+  Dir.chdir("sublime-text-haml-sass") do
+    if RUBY_PLATFORM === "java"
+      ec = FileUtils.mv('SASS', (sublime_packages_path + "/"), :verbose => true, :force => true )
+      check_exit_code(ec)
+    else
+      FileUtils::Verbose.cp_r('SASS', sublime_packages_path)
+    end
+    FileUtils.cp_r('Ruby Haml', sublime_packages_path)
   end
-  check_exit_code()
-  FileUtils.cp_r('Ruby Haml', sublime_packages_path)
-  check_exit_code()
+  puts Msg.new("Installed SASS and Haml highlighting.").color(:green)
 end
-#Dir.chdir(tempdir_path)
-puts Msg.new("Installed SASS and Haml highlighting.").color(:green)
+install_SASS(sublime_packages_path)
 
-#Install Rails Tutorial Snippets
-%x[git clone git@github.com:mhartl/rails_tutorial_snippets.git RailsTutorial]
-check_exit_code()
-FileUtils.rm_r('RailsTutorial/.git') if File.exists?('RailsTutorial/.git')
-check_exit_code()
-FileUtils.cp_r('RailsTutorial', sublime_packages_path)
-check_exit_code()
-puts Msg.new("Installed Rails Tutorial Snippets.").color(:green)
-
-#Install Alternative Auto-completion
-%x[git clone git://github.com/alexstaubo/sublime_text_alternative_autocompletion.git]
-check_exit_code()
-contents_of_directory = Dir.glob('sublime_text_alternative_autocompletion/*.py*')
-contents_of_directory.each do |file|
-  FileUtils.cp(file, sublime_packages_path)
+def install_Snippets(sublime_packages_path)#Install Rails Tutorial Snippets
+  %x[git clone git@github.com:mhartl/rails_tutorial_snippets.git RailsTutorial]
   check_exit_code()
+  FileUtils.rm_r('RailsTutorial/.git') if File.exists?('RailsTutorial/.git')
+  FileUtils.cp_r('RailsTutorial', sublime_packages_path)
+  puts Msg.new("Installed Rails Tutorial Snippets.").color(:green)
 end
-puts Msg.new("Installed Alternative Auto-completion.").color(:green)
+install_Snippets(sublime_packages_path)
 
-#Install RubyTest
-%x[git clone https://github.com/maltize/sublime-text-2-ruby-tests.git RubyTest]
-check_exit_code()
-FileUtils.rm_r('RubyTest/.git') if File.exists?('RubyTest/.git')
-check_exit_code()
-FileUtils.cp_r('RubyTest', sublime_packages_path)
-check_exit_code()
+def install_AltAutoComplete(sublime_packages_path)#Install Alternative Auto-completion
+  %x[git clone git://github.com/alexstaubo/sublime_text_alternative_autocompletion.git]
+  check_exit_code()
+  contents_of_directory = Dir.glob('sublime_text_alternative_autocompletion/*.py*')
+  contents_of_directory.each do |file|
+    FileUtils.cp(file, sublime_packages_path)
+  end
+  puts Msg.new("Installed Alternative Auto-completion.").color(:green)
+end
+install_AltAutoComplete(sublime_packages_path)
 
-def update_theme_file(path)
+def install_RubyTest(sublime_packages_path)#Install RubyTest
+  %x[git clone https://github.com/maltize/sublime-text-2-ruby-tests.git RubyTest]
+  check_exit_code()
+  FileUtils.rm_r('RubyTest/.git') if File.exists?('RubyTest/.git')
+  FileUtils.cp_r('RubyTest', sublime_packages_path)
+end
+install_RubyTest(sublime_packages_path)
+
+def update_theme_file(path) #THEMES
   puts 'Updating file "Theme - Default/widget.sublime-settings" with new settings'
   file_contents = File.read(path +"/Theme - Default/Widget.sublime-settings").gsub(
     "Packages/Theme - Default/Widgets.stTheme",
@@ -117,15 +145,14 @@ def update_theme_file(path)
   File.open(path + "/Theme - Default/Widget.sublime-settings", "w+") do |file| 
     file.write(file_contents)
   end
-  check_exit_code()
 end
+#Logic Here is weird fix up.
 update_theme_file(sublime_packages_path) if File.exists?(sublime_packages_path + \
   "/Theme - Default/Widget.sublime-settings")
 puts Msg.new("Installed RubyTest.").color(:green)
 
-#Instal Coffee-Script Build
-options = {:coffee_script => :false}
-if options[:coffee_script] && !`which coffee`.empty?
+#Instal Coffee-Script Build ##Still TESTING this
+def install_CoffeeScriptBuild(sublime_packages_path)
   coffee_path = `which coffee`.chomp.split("/")[0...-1].join("/") 
   Dir.mkdir("#{sublime_packages_path}/CoffeeScript")
   File.open("#{sublime_packages_path}/CoffeeScript/CoffeeScript.sublime-build", "w+") do
@@ -133,29 +160,37 @@ if options[:coffee_script] && !`which coffee`.empty?
   end unless File.exists?("#{sublime_packages_path}/CoffeeScript/CoffeeScript.sublime-build")
   puts Msg.new("Installed CoffeeScript Build").color(:green)
 end
+if options[:coffee_script] && !`which coffee`.empty?
+  install_CoffeeScriptBuild(sublime_packages_path)
+end
 
 #Ones that install on the user path
-#Install Auxiliary files rails_tutorial_sublime_text
-%x[git clone git@github.com:mhartl/rails_tutorial_sublime_text.git]
-contents_of_directory = Dir.glob('rails_tutorial_sublime_text/*')
-contents_of_directory.each do |file|
-  FileUtils.cp(file , sublime_user_packages_path)
-  check_exit_code()
+
+def install_AuxFiles(sublime_user_packages_path) #AUX Files
+  #Install Auxiliary files rails_tutorial_sublime_text
+  %x[git clone git@github.com:mhartl/rails_tutorial_sublime_text.git]
+  contents_of_directory = Dir.glob('rails_tutorial_sublime_text/*')
+  contents_of_directory.each do |file|
+    FileUtils.cp(file , sublime_user_packages_path)
+    #Can this copy be changed for install??
+  end
+  puts Msg.new("Installed Auxiliary files for rails tutorial.").color(:green)
 end
-puts Msg.new("Installed Auxiliary files for rails tutorial.").color(:green)
+install_AuxFiles(sublime_user_packages_path)
 
-#Install Sublime ERB 
-%x[git clone git@github.com:eddorre/SublimeERB.git sublime_erb]
-check_exit_code()
-FileUtils.rm_r('sublime_erb/.git') if File.exists?('sublime_erb/.git')
-check_exit_code()
-FileUtils.cp_r('sublime_erb', sublime_user_packages_path)
-check_exit_code()
-puts Msg.new("Installed Sublime ERB (needs manual action below to complete).").color(:yellow)
-puts Msg.new("open the Preferences>KeyBindings-User file and add: ").color(:yellow)
-puts %Q`{ "keys": ["ctrl+shift+."], "command": "erb" }`
+def install_ERB(sublime_user_packages_path) #Install Sublime ERB 
+  %x[git clone git@github.com:eddorre/SublimeERB.git sublime_erb]
+  check_exit_code()
+  FileUtils.rm_r('sublime_erb/.git') if File.exists?('sublime_erb/.git')
+  FileUtils.cp_r('sublime_erb', sublime_user_packages_path)
+  puts Msg.new("Installed Sublime ERB (needs manual action below to complete).").color(:yellow)
+  puts Msg.new("open the Preferences>KeyBindings-User file and add: ").color(:yellow)
+  puts %Q`{ "keys": ["ctrl+shift+."], "command": "erb" }`
+end
+install_ERB(sublime_user_packages_path)
 
-if RUBY_PLATFORM == "java"
+#Clean up TestDirectory if JRuby due to non-actual-temp dir location
+if RUBY_PLATFORM == "java" or options[:cleanup]
   puts Msg.new("Cleaning up Temp Directory").color(:blue)
-  FileUtils.rm_rf(tempdir_path, :verbose => true) if RUBY_PLATFORM == "java"
+  FileUtils.rm_rf(tempdir_path, :verbose => true)
 end
